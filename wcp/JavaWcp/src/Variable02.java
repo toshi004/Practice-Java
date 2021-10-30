c148e411166a7defb59050917bf556d2832b2330
ToshikiIwabuchi <d.toshi004.35@gmail.com>
2021-10-30 22:35:23 +0900

    [Add]変数と定数

---
+++ wcp/JavaWcp/src/Variable02.java
@@ -0,0 +1,11 @@
+
+public class Variable02 {
+
+	public static void main(String[] args) {
+		final int MIN_NUMBER = 10;
+		System.out.println(MIN_NUMBER);
+		
+//		再代入しようとするとエラーになる
+//		MIN_NUMBER = 20;
+	}
+}
