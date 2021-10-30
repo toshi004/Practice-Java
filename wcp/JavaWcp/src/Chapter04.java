42e706510c19af8de134a21cba7de461e3a2f093
ToshikiIwabuchi <d.toshi004.35@gmail.com>
2021-10-30 22:44:48 +0900

    [Update]変数と定数テスト

--- wcp/JavaWcp/src/Chapter04.java
+++ wcp/JavaWcp/src/Chapter04.java
@@ -2,13 +2,12 @@
 public class Chapter04 {
 
 	public static void main(String[] args) {
+		final int LUCKY_NUMBER = 4;
 		String name = "Toshiki";
 		System.out.println(name);
 
 		name = "Ricky";
 		System.out.println(name);
-
-		final int LUCKY_NUMBER = 4;
 		System.out.println(LUCKY_NUMBER);
 	}
 }
