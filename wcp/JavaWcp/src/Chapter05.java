7ab5ab82632c30891199c87bc9696e447f313216
ToshikiIwabuchi <d.toshi004.35@gmail.com>
2021-11-02 22:41:45 +0900

    [Add]型

---
+++ wcp/JavaWcp/src/Chapter05.java
@@ -0,0 +1,21 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		long l = 12345678901234567L;
+		System.out.println(l);
+		double d = 1.53;
+		System.out.println(d);
+		boolean b = true;
+		System.out.println(b);
+
+		Float f = 1.23456f;
+		System.out.println(f);
+		Integer i = 4;
+		System.out.println(i);
+		Character c = 'あ';
+		System.out.println(c);
+
+		int[] array = new int[] { 10, 20, 30, 40, 50 };
+		System.out.println(array[3]);
+	}
+}
